650d99e43bf51002e627252c80783a1ffe1a75a8
Piswap <133732880+Piswapdefi@users.noreply.github.com>
2023-08-30 02:33:11 +0700

    Update index.js

--- projects/piswap/index.js
+++ projects/piswap/index.js
@@ -1,15 +1,3 @@
-const { getUniTVL } = require("../helper/unknownTokens.js");
+const { uniTvlExport } = require('../helper/unknownTokens')
 
-const factory = "0xf6408ddc67210E91A5cA5a82a8194b87465CE204";
-
-module.exports = {
-  misrepresentedTokens: true,
-  methodology: 'TVL counts the liquidity of the DEX. The factory address() is used to find every LP pair that has been created.',
-  shibarium: {
-    tvl: getUniTVL({
-      chain: 'shibarium',
-      factory,
-      useDefaultCoreAssets: true,
-    }),
-  },
-};
+module.exports = uniTvlExport('shibarium', '0xf6408ddc67210E91A5cA5a82a8194b87465CE204', { fetchBalances: true, })
